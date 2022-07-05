b9c052c72477556858653cc2b11296759e2c1b92
sky <sky@sky.com>
2022-07-05 18:04:37 +0800

    更新了备份的数据库

--- src/main/chat.sql
+++ src/main/chat.sql
@@ -1,73 +1,97 @@
-/*
- Navicat Premium Data Transfer
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: chat
+-- ------------------------------------------------------
+-- Server version	8.0.29
 
- Source Server         : mysql
- Source Server Type    : MySQL
- Source Server Version : 80029
- Source Host           : localhost:3306
- Source Schema         : chat
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 80029
- File Encoding         : 65001
+--
+-- Table structure for table `chatgroup`
+--
 
- Date: 02/07/2022 11:26:47
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
-CREATE DATABASE IF NOT EXISTS chat;
-USE chat;
-
--- ----------------------------
--- Table structure for chatgroup
--- ----------------------------
-CREATE TABLE IF NOT EXISTS `chatgroup`  (
-  `name` char(25) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `password` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `introduce` char(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `chatgroup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chatgroup` (
+  `name` char(25) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
+  `password` char(20) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
+  `introduce` char(200) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for member
--- ----------------------------
-CREATE TABLE IF NOT EXISTS `member`  (
-  `owner` bigint UNSIGNED NULL DEFAULT NULL,
-  `chatgroup` bigint UNSIGNED NULL DEFAULT NULL,
-  INDEX `chatgroup`(`chatgroup` ASC) USING BTREE,
-  INDEX `owner`(`owner` ASC) USING BTREE,
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `owner` bigint unsigned DEFAULT NULL,
+  `chatgroup` bigint unsigned DEFAULT NULL,
+  KEY `chatgroup` (`chatgroup`) USING BTREE,
+  KEY `owner` (`owner`) USING BTREE,
   CONSTRAINT `chatgroup` FOREIGN KEY (`chatgroup`) REFERENCES `chatgroup` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `owner` FOREIGN KEY (`owner`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for report
--- ----------------------------
-CREATE TABLE IF NOT EXISTS `report`  (
-  `chatgroup` bigint UNSIGNED NOT NULL,
-  `userid` bigint UNSIGNED NOT NULL,
-  `value` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `send_time` datetime NULL DEFAULT CURRENT_TIMESTAMP,
-  INDEX `re_chatgroup`(`chatgroup` ASC) USING BTREE,
-  INDEX `userid`(`userid` ASC) USING BTREE,
+--
+-- Table structure for table `report`
+--
+
+DROP TABLE IF EXISTS `report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `report` (
+  `chatgroup` bigint unsigned NOT NULL,
+  `userid` bigint unsigned NOT NULL,
+  `value` text CHARACTER SET utf8mb3 COLLATE utf8_general_ci,
+  `send_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  KEY `re_chatgroup` (`chatgroup`) USING BTREE,
+  KEY `userid` (`userid`) USING BTREE,
   CONSTRAINT `re_chatgroup` FOREIGN KEY (`chatgroup`) REFERENCES `chatgroup` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-CREATE TABLE IF NOT EXISTS `user`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` char(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '匿名用户',
-  `password` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '000000',
-  `introduce` char(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `login_code` bigint UNSIGNED NULL DEFAULT NULL,
-  `last_login_time` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(25) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL DEFAULT '匿名用户',
+  `password` char(20) CHARACTER SET utf8mb3 COLLATE utf8_general_ci NOT NULL DEFAULT '000000',
+  `introduce` char(200) CHARACTER SET utf8mb3 COLLATE utf8_general_ci DEFAULT NULL,
+  `login_code` bigint unsigned DEFAULT NULL,
+  `last_login_time` datetime DEFAULT (now()),
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-05 18:00:32
