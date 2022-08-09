548fd2d8a993fdb64dc54e92d5655bbf0d76f7a8
sky <sky@sky.com>
2022-08-09 11:30:29 +0800

    对日志进行了一些细节优化

--- src/myerror/main.go
+++ src/myerror/main.go
@@ -6,11 +6,27 @@ import (
 	"time"
 )
 
+var Months = map[string]int{
+	"January":   1,
+	"February":  2,
+	"March":     3,
+	"April":     4,
+	"May":       5,
+	"June":      6,
+	"July":      7,
+	"August":    8,
+	"September": 9,
+	"October":   10,
+	"November":  11,
+	"December":  12,
+}
+
 // 报告401错误，显示待登录页面
 func Raise401(context *gin.Context, err error) {
 	now := time.Now()
-	ts := fmt.Sprint(now.Year(), "/", now.Month(), "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
-	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO] ", ts, "401ERROR:", err.Error())))
+	month:=Months[time.Now().Month().String()]
+	ts := fmt.Sprint(now.Year(), "/", month, "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
+	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO]", ts, "401ERROR:", err.Error())))
 	if err != nil {
 		fmt.Println("Raise401:无法写入", err)
 	}
@@ -20,8 +36,9 @@ func Raise401(context *gin.Context, err error) {
 // 报告404错误
 func Raise404(context *gin.Context, err error) {
 	now := time.Now()
-	ts := fmt.Sprint(now.Year(), "/", now.Month(), "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
-	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO] ", ts, "404ERROR:", err.Error())))
+	month:=Months[time.Now().Month().String()]
+	ts := fmt.Sprint(now.Year(), "/", month, "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
+	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO]", ts, "404ERROR:", err.Error())))
 	if err != nil {
 		fmt.Println("Raise404:无法写入", err)
 	}
@@ -31,8 +48,9 @@ func Raise404(context *gin.Context, err error) {
 // 报告500错误
 func Raise500(context *gin.Context, err error) {
 	now := time.Now()
-	ts := fmt.Sprint(now.Year(), "/", now.Month(), "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
-	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO] ", ts, "500ERROR:", err.Error())))
+	month:=Months[time.Now().Month().String()]
+	ts := fmt.Sprint(now.Year(), "/", month, "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
+	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO]", ts, "500ERROR:", err.Error())))
 	if err != nil {
 		fmt.Println("Raise500:无法写入", err)
 	}
@@ -48,8 +66,9 @@ func CRaise404(context *gin.Context) {
 func ShowWarning(context *gin.Context, err error, s string) {
 	context.HTML(200, "myerror/show_warning", s)
 	now := time.Now()
-	ts := fmt.Sprint(now.Year(), "/", now.Month(), "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
-	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO] ", ts, "ShowWarning:", err.Error())))
+	month:=Months[time.Now().Month().String()]
+	ts := fmt.Sprint(now.Year(), "/", month, "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
+	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO]", ts, "ShowWarning:", err.Error())))
 	if err != nil {
 		fmt.Println("ShowWarning:无法写入", err)
 	}
@@ -58,8 +77,9 @@ func ShowWarning(context *gin.Context, err error, s string) {
 // 用于向日志文件和控制台输出错误
 func LogError(err error) {
 	now := time.Now()
-	ts := fmt.Sprint(now.Year(), "/", now.Month(), "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
-	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO] ", ts, "LogError:", err.Error())))
+	month:=Months[time.Now().Month().String()]
+	ts := fmt.Sprint(now.Year(), "/", month, "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
+	_, err = gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO]", ts, "LogError:", err.Error())))
 	if err != nil {
 		fmt.Println("LogError:无法写入", err)
 	}
@@ -68,8 +88,9 @@ func LogError(err error) {
 // 用于向日志文件和控制台输出
 func Write(s string) {
 	now := time.Now()
-	ts := fmt.Sprint(now.Year(), "/", now.Month(), "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
-	_, err := gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO] " + ts + "Write:" + s + "\n")))
+	month:=Months[time.Now().Month().String()]
+	ts := fmt.Sprint(now.Year(), "/", month, "/", now.Day(), " - ", now.Hour(), ":", now.Minute(), ":", now.Second(), " ")
+	_, err := gin.DefaultWriter.Write([]byte(fmt.Sprintln("[INFO]" + ts + "Write:" + s + "\n")))
 	if err != nil {
 		fmt.Println("Write:无法写入", err)
 	}
